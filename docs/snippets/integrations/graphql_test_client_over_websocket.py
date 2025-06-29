40824caa19e23f55b800dbe33e8d162d944d7718
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2025-06-29 18:48:30 +0300

    Add channels integration docs

---
+++ docs/snippets/integrations/graphql_test_client_over_websocket.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.asyncio  # Requires the `pytest-asyncio` plugin
+@pytest.mark.django_db(transaction=True)  # For sessions
+async def test_graphql(graphql) -> None:
+    # Setup...
+
+    query = "query { test }"
+    async for response in graphql.over_websocket(query):
+        assert response.data == {"test": "Hello, World!"}
