78f27be7cd6d09aaafc8a710db0aedfe81b99397
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2025-06-30 14:39:39 +0300

    Improve modeltranslation integration

--- undine/integrations/modeltranslation.py
+++ undine/integrations/modeltranslation.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
+from contextlib import suppress
 from typing import TYPE_CHECKING, TypeGuard
 
 if TYPE_CHECKING:
     from django.db.models import Field, Model
+
+
+with suppress(ImportError):
+    from modeltranslation.fields import TranslationField
+    from modeltranslation.manager import get_translatable_fields_for_model
 
 
 __all__ = [
@@ -12,21 +18,19 @@ __all__ = [
 ]
 
 
-try:
-    from modeltranslation.fields import TranslationField
-except ImportError:
-    TranslationField = type("TranslationField", (), {})  # type: ignore[misc,assignment]
+IS_MODELTRANSLATION_INSTALLED: bool = "TranslationField" in globals()
 
 
 def is_translation_field(field: Field) -> TypeGuard[TranslationField]:
+    if not IS_MODELTRANSLATION_INSTALLED:
+        return False
+
     return isinstance(field, TranslationField)
 
 
 def get_translatable_fields(model: type[Model]) -> set[str]:
     """If `django-modeltranslation` is installed, find all translatable fields in the given model."""
-    try:
-        from modeltranslation.manager import get_translatable_fields_for_model  # noqa: PLC0415
-    except ImportError:
+    if not IS_MODELTRANSLATION_INSTALLED:
         return set()
 
     return set(get_translatable_fields_for_model(model) or [])
