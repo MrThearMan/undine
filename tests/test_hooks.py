164b9cf21d1d2836512e93165014126a8b758f78
Matti Lamppu <lamppu.matti.akseli@gmail.com>
2025-07-04 18:39:24 +0300

    Fix error handling of lifecycle hooks in async context

--- tests/test_hooks.py
+++ tests/test_hooks.py
@@ -1,11 +1,17 @@
 from __future__ import annotations
 
-from typing import Generator
+from typing import AsyncGenerator, Generator
 
 import pytest
 from graphql import ExecutionResult, GraphQLError
 
-from undine.hooks import LifecycleHook, LifecycleHookContext, LifecycleHookManager, use_lifecycle_hooks_sync
+from undine.hooks import (
+    LifecycleHook,
+    LifecycleHookContext,
+    LifecycleHookManager,
+    use_lifecycle_hooks_async,
+    use_lifecycle_hooks_sync,
+)
 
 
 def get_default_context() -> LifecycleHookContext:
@@ -82,7 +88,7 @@ def test_lifecycle_hook_manager() -> None:
     assert call_stack == ["my before", "your before", "inside", "your after", "my after"]
 
 
-def test_use_lifecycle_hooks() -> None:
+def test_use_lifecycle_hooks_sync() -> None:
     call_stack: list[str] = []
 
     class MyHook(LifecycleHook):
@@ -102,7 +108,7 @@ def test_use_lifecycle_hooks() -> None:
     assert call_stack == ["before", "inside", "after"]
 
 
-def test_use_lifecycle_hooks__non_graphql_error_raised() -> None:
+def test_use_lifecycle_hooks_sync__error_raised() -> None:
     call_stack: list[str] = []
 
     class MyHook(LifecycleHook):
@@ -127,13 +133,12 @@ def test_use_lifecycle_hooks__non_graphql_error_raised() -> None:
     assert ctx.result is None
 
 
-def test_use_lifecycle_hooks__non_graphql_error_raised__catch() -> None:
+def test_use_lifecycle_hooks_sync__error_raised__catch() -> None:
     call_stack: list[str] = []
 
     class MyHook(LifecycleHook):
         def run(self) -> Generator[None, None, None]:
             call_stack.append("before")
-
             try:
                 yield
             except ValueError as error:
@@ -152,6 +157,174 @@ def test_use_lifecycle_hooks__non_graphql_error_raised__catch() -> None:
 
     assert call_stack == ["before", "inside", "after"]
 
-    assert ctx.result is not None
+    assert isinstance(ctx.result, ExecutionResult)
+    assert ctx.result.data is None
+    assert ctx.result.errors == [GraphQLError("Error")]
+
+
+@pytest.mark.asyncio
+async def test_use_lifecycle_hooks_async() -> None:
+    call_stack: list[str] = []
+
+    class MyHook(LifecycleHook):
+        def run(self) -> Generator[None, None, None]:
+            call_stack.append("before")
+            yield
+            call_stack.append("after")
+
+    @use_lifecycle_hooks_async(hooks=[MyHook])
+    async def func(context: LifecycleHookContext) -> None:  # noqa: RUF029
+        call_stack.append("inside")
+
+    ctx = get_default_context()
+
+    await func(ctx)
+
+    assert call_stack == ["before", "inside", "after"]
+
+
+@pytest.mark.asyncio
+async def test_use_lifecycle_hooks_async__error_raised() -> None:
+    call_stack: list[str] = []
+
+    class MyHook(LifecycleHook):
+        def run(self) -> Generator[None, None, None]:
+            call_stack.append("before")
+            yield
+            call_stack.append("after")
+
+    @use_lifecycle_hooks_async(hooks=[MyHook])
+    async def func(context: LifecycleHookContext) -> None:  # noqa: RUF029
+        call_stack.append("inside")
+        msg = "Error"
+        raise ValueError(msg)
+
+    ctx = get_default_context()
+
+    with pytest.raises(ValueError, match="Error"):
+        await func(ctx)
+
+    assert call_stack == ["before", "inside"]
+
+
+@pytest.mark.asyncio
+async def test_use_lifecycle_hooks_async__error_raised__catch() -> None:
+    call_stack: list[str] = []
+
+    class MyHook(LifecycleHook):
+        def run(self) -> Generator[None, None, None]:
+            call_stack.append("before")
+            try:
+                yield
+            except ValueError as error:
+                call_stack.append("after")
+                self.context.result = ExecutionResult(errors=[GraphQLError(str(error))])
+
+    @use_lifecycle_hooks_async(hooks=[MyHook])
+    async def func(context: LifecycleHookContext) -> None:  # noqa: RUF029
+        call_stack.append("inside")
+        msg = "Error"
+        raise ValueError(msg)
+
+    ctx = get_default_context()
+
+    await func(ctx)
+
+    assert call_stack == ["before", "inside", "after"]
+
+    assert isinstance(ctx.result, ExecutionResult)
+    assert ctx.result.data is None
+    assert ctx.result.errors == [GraphQLError("Error")]
+
+
+@pytest.mark.asyncio
+async def test_use_lifecycle_hooks_async__run_async() -> None:
+    call_stack: list[str] = []
+
+    class MyHook(LifecycleHook):
+        def run(self) -> Generator[None, None, None]:
+            call_stack.append("before")
+            yield
+            call_stack.append("after")
+
+        async def run_async(self) -> AsyncGenerator[None, None]:
+            call_stack.append("before async")
+            yield
+            call_stack.append("after async")
+
+    @use_lifecycle_hooks_async(hooks=[MyHook])
+    async def func(context: LifecycleHookContext) -> None:  # noqa: RUF029
+        call_stack.append("inside")
+
+    ctx = get_default_context()
+
+    await func(ctx)
+
+    assert call_stack == ["before async", "inside", "after async"]
+
+
+@pytest.mark.asyncio
+async def test_use_lifecycle_hooks_async__run_async__error_raised() -> None:
+    call_stack: list[str] = []
+
+    class MyHook(LifecycleHook):
+        def run(self) -> Generator[None, None, None]:
+            call_stack.append("before")
+            yield
+            call_stack.append("after")
+
+        async def run_async(self) -> AsyncGenerator[None, None]:
+            call_stack.append("before async")
+            yield
+            call_stack.append("after async")
+
+    @use_lifecycle_hooks_async(hooks=[MyHook])
+    async def func(context: LifecycleHookContext) -> None:  # noqa: RUF029
+        call_stack.append("inside")
+        msg = "Error"
+        raise ValueError(msg)
+
+    ctx = get_default_context()
+
+    with pytest.raises(ValueError, match="Error"):
+        await func(ctx)
+
+    assert call_stack == ["before async", "inside"]
+
+
+@pytest.mark.asyncio
+async def test_use_lifecycle_hooks_async__run_async__error_raised__catch() -> None:
+    call_stack: list[str] = []
+
+    class MyHook(LifecycleHook):
+        def run(self) -> Generator[None, None, None]:
+            call_stack.append("before")
+            try:
+                yield
+            except ValueError as error:
+                call_stack.append("after")
+                self.context.result = ExecutionResult(errors=[GraphQLError(str(error))])
+
+        async def run_async(self) -> AsyncGenerator[None, None]:
+            call_stack.append("before async")
+            try:
+                yield
+            except ValueError as error:
+                call_stack.append("after async")
+                self.context.result = ExecutionResult(errors=[GraphQLError(str(error))])
+
+    @use_lifecycle_hooks_async(hooks=[MyHook])
+    async def func(context: LifecycleHookContext) -> None:  # noqa: RUF029
+        call_stack.append("inside")
+        msg = "Error"
+        raise ValueError(msg)
+
+    ctx = get_default_context()
+
+    await func(ctx)
+
+    assert call_stack == ["before async", "inside", "after async"]
+
+    assert isinstance(ctx.result, ExecutionResult)
     assert ctx.result.data is None
     assert ctx.result.errors == [GraphQLError("Error")]
