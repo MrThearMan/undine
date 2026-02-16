e761b63371382103e34b13a13b5608295607b959
Matti Lamppu <matti.lamppu@vincit.fi>
2026-02-16 12:40:32 +0200

    Add testing tools for GraphQL over SSE

--- example_project/app/urls.py
+++ example_project/app/urls.py
@@ -6,4 +6,7 @@ from example_project.app import views
 
 urlpatterns = [
     path("websocket-testing/", views.websocket_testing, name="websocket_testing"),
+    path("sse-testing/", views.sse_testing, name="sse_testing"),
+    path("sse-testing/dc/", views.sse_testing_dc, name="sse_testing_dc"),
+    path("sse-testing/sc/", views.sse_testing_sc, name="sse_testing_sc"),
 ]
--- example_project/app/views.py
+++ example_project/app/views.py
@@ -2,7 +2,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from django.http import HttpResponsePermanentRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from undine.settings import undine_settings
 
@@ -15,3 +17,15 @@ def websocket_testing(request: HttpRequest) -> HttpResponse:
         "ws_path": undine_settings.WEBSOCKET_PATH,
     }
     return render(request, "app/websocket_testing.html", context=context)
+
+
+def sse_testing(request: HttpRequest) -> HttpResponse:
+    return HttpResponsePermanentRedirect(redirect_to=reverse("sse_testing_dc"))
+
+
+def sse_testing_dc(request: HttpRequest) -> HttpResponse:
+    return render(request, "app/sse_testing_dc.html")
+
+
+def sse_testing_sc(request: HttpRequest) -> HttpResponse:
+    return render(request, "app/sse_testing_sc.html")
