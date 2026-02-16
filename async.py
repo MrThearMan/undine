0b9de4ad241b0647302d9560cea40ff62ce0c4d5
Matti Lamppu <matti.lamppu@vincit.fi>
2026-02-16 11:43:20 +0200

    Use Granian for async server

--- async.py
+++ async.py
@@ -2,8 +2,14 @@ from __future__ import annotations
 
 import os
 
-import uvicorn
+from granian import Granian
+from granian.constants import Interfaces
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.project.settings")
-    uvicorn.run("example_project.project.asgi:application", host="localhost", port=8000, reload=True)
+    Granian(
+        target="example_project.project.asgi:application",
+        port=8000,
+        interface=Interfaces.ASGI,
+        log_level="error",
+    ).serve()
