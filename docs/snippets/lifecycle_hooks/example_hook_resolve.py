1dff8a459f5fb3a627b2153114fef6041ca330dd
Matti Lamppu <matti.lamppu@vincit.fi>
2026-03-23 08:30:13 +0200

    Add example of how to hook into async resolvers

--- docs/snippets/lifecycle_hooks/example_hook_resolve.py
+++ docs/snippets/lifecycle_hooks/example_hook_resolve.py
@@ -1,6 +1,8 @@
+from collections.abc import Awaitable
 from typing import Any
 
 from graphql import GraphQLFieldResolver
+from graphql.pyutils import AwaitableOrValue
 
 from undine import GQLInfo
 from undine.hooks import LifecycleHook
@@ -9,9 +11,22 @@ from undine.hooks import LifecycleHook
 class ExampleHook(LifecycleHook):
     """Example hook"""
 
-    # The 'resolve' hook only has a synchronous interface.
-    def resolve(self, resolver: GraphQLFieldResolver, root: Any, info: GQLInfo, **kwargs: Any) -> Any:
+    # The 'resolve' step only has a synchronous interface.
+    # If you want your hook to also support awaitable resolvers,
+    # you need to check if the resolver returns an awaitable and handle it separately.
+
+    def resolve(self, resolver: GraphQLFieldResolver, root: Any, info: GQLInfo, **kwargs: Any) -> AwaitableOrValue[Any]:
         print("before")
         result = resolver(root, info, **kwargs)
         print("after")
+
+        if info.is_awaitable(result):
+            return self.resolve_async(resolver=result, root=root, info=info, **kwargs)
+
+        return result
+
+    async def resolve_async(self, awaitable: Awaitable[Any], root: Any, info: GQLInfo, **kwargs: Any) -> Any:
+        print("before async")
+        result = await awaitable
+        print("after async")
         return result
