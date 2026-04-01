0827f5e10b3de715ec1b21ba93fd18e14c868ef3
Matti Lamppu <matti.lamppu@vincit.fi>
2026-04-01 12:11:51 +0300

    Get nox django version from classifiers

--- noxfile.py
+++ noxfile.py
@@ -11,26 +11,39 @@ import nox
 logger = logging.getLogger(__name__)
 
 
-@cache
 def python_versions() -> list[str]:
-    pyproject = Path("pyproject.toml").read_text(encoding="utf-8")
-    toml_data = tomllib.loads(pyproject)
-    classifiers: list[str] = toml_data["project"]["classifiers"]
-
     python_version_pattern = re.compile(r"^Programming Language :: Python :: (?P<version>\d+\.\d+)$")
-
-    versions: list[str] = []
-    for classifier in classifiers:
-        match = python_version_pattern.match(classifier)
-        if match is not None:
-            versions.append(match.group("version"))
-
+    versions = get_versions(python_version_pattern)
     logger.debug(f"Python versions: {', '.join(versions)}")
     return versions
 
 
+def django_versions() -> list[str]:
+    django_versions_pattern = re.compile(r"^Framework :: Django :: (?P<version>\d+\.\d+)$")
+    versions = get_versions(django_versions_pattern)
+    logger.debug(f"Django versions: {', '.join(versions)}")
+    return [f"{version}.*" for version in versions]
+
+
+@cache
+def get_classifiers() -> list[str]:
+    pyproject = Path("pyproject.toml").read_text(encoding="utf-8")
+    toml_data = tomllib.loads(pyproject)
+    return toml_data["project"]["classifiers"]
+
+
+def get_versions(pattern: re.Pattern[str]) -> list[str]:
+    classifiers = get_classifiers()
+    versions: list[str] = []
+    for classifier in classifiers:
+        match = pattern.match(classifier)
+        if match is not None:
+            versions.append(match.group("version"))
+    return versions
+
+
 @nox.session(python=python_versions(), reuse_venv=True)
-@nox.parametrize("django", ["5.0.*", "5.1.*", "5.2.*", "6.0.*"])
+@nox.parametrize("django", django_versions())
 @nox.parametrize("graphql_core", ["3.2.*", "3.3.0a12"])
 def tests(session: nox.Session, django: str, graphql_core: str) -> None:
     # Django 6.0 is only supports Python 3.12 and above
